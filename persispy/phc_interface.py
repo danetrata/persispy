754720d158a40e1938002ff6f9c010e83b41a934
callmetaste <danetrata@gmail.com>
2015-10-19 12:57:10 -0700

    more thinking about phc
    Former-commit-id: a4f5e013528b7f1144877fc6155eec2e30b0e27d

--- persispy/phc_interface.py
+++ persispy/phc_interface.py
@@ -21,7 +21,7 @@ phc_cloud(equation, points)
 Given a string equation and a number of lines for intersecting the 
 equation, returns a PointCloud.
 """
-def phc_2d_cloud(eqn = "x^2 + y^2 - 1", intersectWith = "line", nTimes = 10):
+def phc_2d_cloud(eqn = "x^2 + y^2 - 1", intersectWith = "line", nPoints = 10):
 
     # returns a set of points given the raw string from phc
     def phc_2d_out(phcOut):
@@ -60,7 +60,7 @@ def phc_2d_cloud(eqn = "x^2 + y^2 - 1", intersectWith = "line", nTimes = 10):
         return lines
 
     if intersectWith == "line":
-        intersects = random_lines(nTimes)
+        intersects = random_lines(nPoints)
 
     cloudPoints = []
     for intersect in intersects:
@@ -87,77 +87,68 @@ def phc_2d_cloud(eqn = "x^2 + y^2 - 1", intersectWith = "line", nTimes = 10):
 
 
 
-def phc_3d_cloud(eqn = "x^2 + y^2 - 1", intersectWith = "plane", nTimes = 10, LOUD=False):
+def phc_3d_cloud(eqn = "x^2 + y^2 - 1", intersectWith = "plane", nPoints = 10, LOUD=False, treshold = 1.0):
 
-    # returns a set of points given the raw string from phc
-    def phc_3d_out(phcOut):
-        from phcpy.solutions import strsol2dict # points
-        points = []
-        for i in phcOut:
-            d = strsol2dict(i)
-            coords = (d['x'], d['y'], d['z'])
 
-            tres = 1.0
-            if abs(d['x'].imag) > tres or abs(d['y'].imag) > tres or abs(d['z'].imag) > tres:
-                print "too complex"
-                print i
-
-            else:
-                points.append(coords)
-        return points
 
     from phcpy.solver import total_degree
     from phcpy.solver import total_degree_start_system
     from phcpy.trackers import track
     from phcpy.solver import solve
+    from numpy.random import uniform
+    from phcpy.solutions import strsol2dict # points
+    from phcpy.sets import embed
+    
+    def is_float_eq(a, b, epsilon = 1.0):
+        print a,", ",b,",", abs(a-b)
+        print abs(a - b) <= epsilon
+        return abs(a - b) <= epsilon
+
+
+    n = 0
+    phcEqn = eqn+";"
+    points = []
+    while (n <= nPoints):
+        if intersectWith == "plane":
+            a,b,c = uniform(-1,1,size=3)
+            q = str(a)+"*x + "+str(b)+"*y +" +str(c)+ "*z;"
+
+            p = [phcEqn, q]
+            p = embed(3,1,[phcEqn, q])
+            (q, qsols) = total_degree_start_system(p)
+            phcSol = track(p, q, qsols) 
+
+            for i in phcSol:
+                d = strsol2dict(i)
+                x, y, z = (d['x'], d['y'], d['z'])
+                if not is_float_eq(x.real, 0.0) or not is_float_eq(y.real, 0.0) or not is_float_eq(z.real, 0.0):
+                    print "too far"
+                    print i
+                    print (x,y,z)
+
+                else:
+                    points.append((x,y,z))
+                    n += 1
+
+
 
     import numpy as np
-    from numpy.random import uniform
-
     import persispy as persispy
     import point_cloud as point_cloud
     import hash_point as hash_point
     from point_cloud import PointCloud
     from hash_point import HashPoint
 
-    phcEqn = eqn+";"
-
-    intersects = [] 
-    
-    # creating a list of random lines to intersect
-    def random_planes(n):
-        lines = []
-        for item in range(0,n):
-            a,b,c = uniform(-1,1,size=3)
-            line = str(a)+"*x + "+str(b)+"*y +" +str(c)+ "*z;"
-            lines.append(line)
-        return lines
-
-
-    if intersectWith == "plane":
-        intersects = random_planes(nTimes)
-
     cloudPoints = []
-    from phcpy.sets import embed
-    for intersect in intersects:
-        p = [phcEqn, intersect]
-        p = embed(3,1,p)
-        (q, qsols) = total_degree_start_system(p)
-        sol = track(p, q, qsols) 
-
-        if False:
-            for x in sol:
-                print x
-
-        for coord in phc_3d_out(sol):
-            cloudPoints.append(
-                HashPoint(
-                    np.array(
-                        coord, 
-                        dtype=complex
-                    )
+    for coord in points:
+        cloudPoints.append(
+            HashPoint(
+                np.array(
+                    coord, 
+                    dtype=complex
                 )
             )
+        )
     
     
     return point_cloud.PointCloud(cloudPoints) 
