df25943241d566f6a6153d3452ef2410d5e17932
callmetaste <danetrata@gmail.com>
2015-11-06 15:22:35 -0800

    Setup for MCL labs

--- setup.py
+++ setup.py
@@ -2,9 +2,29 @@
 # -*- coding: utf-8 -*-
 
 
-import setuptools
-from setuptools import setup
-print "setup using setuptools"
+from distutils.core import setup
+
+def is_package(path): # find packages helper function
+    return (
+        os.path.isdir(path) and
+        os.path.isfile(os.path.join(path, '__init__.py'))
+    )
+    
+def find_packages(path, base=""): 
+                                  #| given a path,
+                                  #| recursively walks the directory to find
+                                  #| submodules
+    packages = {}
+    for item in os.listdir(path):
+        dir = os.path.join(path, item)
+        if is_package(dir):
+            if base:
+                module_name = "%(base)s.%(item)s" % vars()
+            else:
+                module_name = item
+            packages[module_name] = dir
+            packages.update(find_packages(dir, module_name))
+    return packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -28,7 +48,6 @@ setup(
     author="Benjamin Antieau",
     author_email='benjamin.antieau@gmail.com',
     url='https://github.com/benjaminantieau/persispy',
-    packages = setuptools.find_packages(),
     platforms='any',
     install_requires=requirements,
     license="BSD",
