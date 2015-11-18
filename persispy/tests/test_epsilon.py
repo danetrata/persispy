0b2d393a2b0233747a14e92e71c212cfee985133
callmetaste <danetrata@gmail.com>
2015-11-17 22:39:54 -0800

    Adding a possible implementation of interface_class.py.
    Former-commit-id: 74abe1d139666729c344d5a8199b993eb6aab658

--- persispy/tests/test_epsilon.py
+++ persispy/tests/test_epsilon.py
@@ -1,16 +1,20 @@
 # TODO: epsilon is a function of coefficients, number of points, and degree
 from persispy.phc.interface import phc_cloud
-from persispy.weighted_simplicial_complex import wGraph
+from persispy.point_cloud import PointCloud
+from persispy.weighted_simplicial_complex import wSimplex, wGraph, wSimplicialComplex
 
 pointDict = {
+    "circle": "x^2 + y^2 - 1",
     "sphere": "x^2 + y^2 + z^2 - 1",
     "torus": "16*x^2 + 16*y^2 - (x^2 + y^2 + z^2 + 3)^2",
     "hyperbolid": "x^2 + y^2 - z^2 - 1"
 }
 
 def main():
-    phcCloud = phc_cloud(eqn = pointDict["sphere"], num_points = 500) 
-    wGraph.neighborhood_graph(wGraph(phcCloud),0.1)
+    phcCloud = phc_cloud(eqn = pointDict["circle"], num_points = 1000) 
+    ng = phcCloud.neighborhood_graph(.01, "subdivision")
+    print ng
+    print ng.connected_components_1()
 
 
 
