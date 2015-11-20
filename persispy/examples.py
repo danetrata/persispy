b91f62e52a3f7b74bcb6747d36c23ed96ead3235
Mason Boeman <maboesanman@gmail.com>
2015-11-20 17:14:42 -0600

    small persistent homology fixes and moved hashpoint from examples.py to hash_point.py
    Former-commit-id: 5409362378820a2c428ac688cd9973fabea89de3

--- persispy/examples.py
+++ persispy/examples.py
@@ -23,31 +23,6 @@ import scipy.constants as scic
 import point_cloud as point_cloud
 import hashlib as hashlib
 
-class HashPoint:
-    '''
-    A wrapped numpy array to allow hashing.
-
-    Vars: _coords (a numpy array).
-
-    EXAMPLES:
-    >>> HashPoint([1,2,3])
-    array([1, 2, 3])
-    '''
-    def __init__(self,coords,index):
-        self._coords=np.array(coords)
-        self._index=index
-    def __hash__(self):
-        try:
-            out=self._hash
-            return out
-        except:
-            self._hash=int(hashlib.sha1(self._coords.view()).hexdigest(),16)
-            return self._hash
-
-    def __repr__(self):
-        return "point "+str(self._index)+": "+str(self._coords.__repr__())[6:-1]
-    def __cmp__(self,other):
-        return self._index.__cmp__(other._index)
 
 def points_2sphere(num_points,radius=1,method='normalized'):
     '''
