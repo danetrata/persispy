25fdcb5af3dd93236abb25017dd41f875860e0b4
Mason S Boeman <boeman2@math.uic.edu>
2016-03-28 19:19:29 -0500

    added color to the barcode plot

--- persispy/persistent_homology.py
+++ persispy/persistent_homology.py
@@ -2,15 +2,19 @@ import sortedcontainers
 from collections import defaultdict
 import numpy as np
 import itertools as it
-from matplotlib import pyplot as plt
-import persispy.weighted_simplicial_complex as wsc
-DEBUG = False
+import weighted_simplicial_complex as wsc
+import matplotlib.pyplot as plt
+import colorsys
 
 class PersistentHomology:
     def __init__(self, simplicial_complex, n):
         self.Currentindex=0
         self.VertexDict = dict() #look up simplex container from tuple of vertices
-        self.Simplices = self.sort_simplices(simplicial_complex, n)
+        _wSimplices = [] 
+        for dimension in simplicial_complex._simplices:
+            if dimension<=n+1:
+                _wSimplices.extend(simplicial_complex._simplices[dimension])
+        self.Simplices = sorted([SimplexContainer(s) for s in sorted(_wSimplices)])
         self.PersistencePairs = dict()
         for i,s in enumerate(self.Simplices):
             self.VertexDict[tuple(s.simplex._vertices)] = s
@@ -31,19 +35,6 @@ class PersistentHomology:
                     #print 'pair: '+ str(s.index)+' '+str(s.entries[-1].index)+' '+str(-s.entries[-1].simplex._weight+s.simplex._weight)
                     self.PersistencePairs[s.entries[0]]=s
             
-    def sort_simplices(self, simplicial_complex, n):
-        _wSimplices = [] 
-        for dimension in simplicial_complex._simplices:
-            if dimension<=n+1:
-                _wSimplices.extend(simplicial_complex._simplices[dimension])
-#         if DEBUG:
-#             for item in _wSimplices:
-#                 print(item)
-        simContainer = [SimplexContainer(s) for s in sorted(_wSimplices)]
-#         if DEBUG: print(simContainer)
-        return sorted(simContainer)
-
-
     def plotBarCode(self,d,e):
         i=1
         j=1;
@@ -58,10 +49,10 @@ class PersistentHomology:
                     if len(s.simplex._vertices)==j:
                         if s.simplex._weight!=self.PersistencePairs[s].simplex._weight:
                             y=i
-                            c = 1-(self.PersistencePairs[s].simplex._weight-s.simplex._weight)/e
+                            a = 1-(self.PersistencePairs[s].simplex._weight-s.simplex._weight)/e
+                            rgb = colorsys.hls_to_rgb(len(s.simplex._vertices)*(3-5**.5)*.5,.5,.5)
                             i=i+1
-                            plt.plot([s.simplex._weight,self.PersistencePairs[s].simplex._weight],[y,y],color='black',linestyle='-', linewidth=10)
-#                             plt.plot([s.simplex._weight,self.PersistencePairs[s].simplex._weight],[y,y],color=(c,c,c,1-c),linestyle='-', linewidth=10)
+                            plt.plot([s.simplex._weight,self.PersistencePairs[s].simplex._weight],[y,y],color=(rgb[0],rgb[1],rgb[2],1-a),linestyle='-', linewidth=1)
             j=j+1
             i=i+100
             
@@ -73,17 +64,16 @@ class SimplexContainer:
         self.simplex = sim
         self.entries = sortedcontainers.SortedSet()
         self.index = -1
-
     def compute_entries(self, ph):
         if len(self.simplex._vertices)<2:
             return
         for x in range(len(self.simplex._vertices)):
             # Make more pythonic!
-            self.entries.add(ph.VertexDict[tuple(self.simplex._vertices[:x]+self.simplex._vertices[x:])])
+            self.entries.add(ph.VertexDict[tuple(self.simplex._vertices[:x]+self.simplex._vertices[x+1:])])
             
     def __hash__(self):
         return hash(tuple(self.simplex._vertices))
-
+    
     def __lt__(self,other):
         return self.simplex < other.simplex
 
